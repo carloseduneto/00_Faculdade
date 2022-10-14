98fa70cb3507dfd38ba5f54f5f115676480f6096
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2022-10-14 18:55:12 -0300

    Sistemas 2

---
+++ 02 Web JavaScript/06_index.js
@@ -0,0 +1,9 @@
+
+var Numero1= prompt("Digite um numero: ");
+var Numero2= prompt("Digite outro numero: ");
+window.alert (`O valores são ${Numero1} e ${Numero2}`)
+window.alert(`A soma de ${Numero1} e ${Numero2} é  ${Numero1+Numero2} `)
+window.alert(`A subtração de ${Numero1} e ${Numero2} é  ${Numero1-Numero2} `)
+window.alert(`A multiplicação de ${Numero1} e ${Numero2} é  ${Numero1*Numero2} `)
+window.alert(`A divisão de ${Numero1} e ${Numero2} é  ${Numero1/Numero2} `)
+window.alert(`A resto de ${Numero1} e ${Numero2} é  ${Numero1%Numero2} `)
