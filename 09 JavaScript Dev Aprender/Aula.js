e0d17217fdb45f3bcf0b46d27bb79a2cad9f0e4b
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2022-10-14 23:44:15 -0300

    Sistemas 2

---
+++ 09 JavaScript Dev Aprender/Aula.js
@@ -0,0 +1,60 @@
+// console.log("Carlos")
+// console.log("Bem vindo a todos")
+// console.log("🗡️Shovel Knight🛡️")
+// //Pela nonagésima vez o que é uma variável?
+// // Receber um valor em algum luagr do meu programa
+
+// let idade=5;
+// //let é o tipo
+// //var é a versão mais antiga
+// console.log(idade);
+// let OutroValor=9e+5;
+// console.log(OutroValor)
+
+//Não dar o nome de variáveis com números, espaços
+
+//Usar camelCase (minúsculo 1º e o resto a 1º maiúscula)
+
+let nome= "Carlos Eduardo";
+let idade= 21;
+let comoEstaSeSentido= "Empolgado";
+
+console.log(nome, idade, comoEstaSeSentido)
+
+let pensamento= "I'm unstoppable today";
+pensamento="I'm porche without no breaks";
+console.log(pensamento)
+
+//o let pode redefinir sempre o valor
+
+//const sempre é constante, não pode mudar valores
+
+
+//Tipos primitivos 
+let nomeA = "Fernando <3"; //String literal, juntando várias letras
+
+let idadeA = 18; //number literal
+
+let estaAprovado = true; //boolean
+
+let sobrenome = undefined;  //Undefined
+
+let corSelecionada = null; //Resetar o valor toda vez
+
+
+//Tipagem dinânimica ele descobre em tempo de execução :)
+
+//O que é um objeto? Ele junmta as informações em um lugar só
+
+let pessoa ={
+    nome: "Fernado <3",
+    idade: 18,
+    estaAprovado: true,
+    sobrenome: "???",
+    educacao: "Ensino Médio",
+    endereco: "Rua Almeida, 55",
+    bairro: "Jardim das Flores"
+
+}; //Chave e valor, estilo dicionário e arrays associativos
+
+console.log(pessoa);
