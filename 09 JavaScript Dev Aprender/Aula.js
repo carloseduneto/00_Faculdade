e09832b696f85bd92eb92560d5f240fa76d17fce
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2022-10-15 23:09:58 -0300

    Sistemas 2

--- 09 JavaScript Dev Aprender/Aula.js
+++ 09 JavaScript Dev Aprender/Aula.js
@@ -58,3 +58,121 @@ let pessoa ={
 }; //Chave e valor, estilo dicionário e arrays associativos
 
 console.log(pessoa);
+
+
+//Arreios (Arrays): conjunto de dados que pode ser acessado com um índice
+
+let familia = [22,222,"Saudades",true]
+console.log(familia.length)
+console.log(familia[3])
+
+let nomeDoAmigo=["Rafael", "34", "São Sebastião do Paraíso"]
+console.log(nomeDoAmigo)
+
+
+//Funções: Verbo + Substantivo
+let corSite = "azul";
+function resetaCor (cor, tonalidade){
+corSite= cor + " " + tonalidade;
+
+}; //+ concatena valores
+
+
+console.log(corSite);
+resetaCor("cinza", "claro");
+console.log(corSite)
+
+
+produtoSelecionado= "Camiseta"
+function mudarProduto (produto){
+    produtoSelecionado = produto;
+};
+
+console.log(produtoSelecionado);
+mudarProduto("Calça")
+console.log(produtoSelecionado)
+
+
+//Tipos de funções: realiza uma tarefa e não devolve nada, não usa o retorno
+
+function mostrarNaTela(){
+    console.log("Diego")
+}
+mostrarNaTela();
+
+function raizQuadrada(valor){
+    return valor ** (1/2)
+};
+console.log(raizQuadrada(81))
+
+
+let resultado = raizQuadrada(16)
+console.log (resultado)
+
+
+//Operadores Aritiméticos (Matemáticos)
+let salario = 100;
+
+// + - * / ** 
+console.log(salario+salario)
+console.log(salario-salario)
+console.log(salario*salario)
+console.log(salario/salario)
+console.log(salario**salario)
+
+// ++ --
+let idadeB = 18;
+console.log(++idadeB)
+console.log(idadeB)
+
+console.log(--idadeB)
+console.log(--idadeB)
+
+//Operadores de atribuição
+let valorDaCasa= 100;
+//valorDaCasa = valorDaCasa + valorDaCasa
+valorDaCasa +=valorDaCasa
+//valorDaCasa = valorDaCasa - valorDaCasa
+valorDaCasa -=valorDaCasa
+console.log(valorDaCasa);
+
+
+
+
+//Operadores de comparação
+//Igualdade Estrita 
+
+console.log (1 === 1 ) //compara valores e tipos
+console.log ("1" === 1 )
+
+//Igualdade solta
+console.log (1 == 1)
+console.log ("1" ==1) //compara apenas valores, apenas converte
+
+//Operadores lógicos
+//Tem um cliente, 100 premium, comum
+let pontos = 100;
+let tipo = pontos > 100 ? "premium" : "comum"; //primeiro é vdd e o segundo falso
+console.log(tipo); 
+
+//Operadores Bitwise (lógico)
+//Operador lógivco e (&&)
+//Retorna TRUE se os dois operando forem 
+
+// console.log(true&&false);
+
+let maiorDeIdade=true;
+let possuiCarteiraDeTrabalho= true;
+
+let podeAplicarAVaga= maiorDeIdade && possuiCarteiraDeTrabalho;
+console.log(podeAplicarAVaga)
+
+//Operador lógico ou (||)
+//Retorna true se pelo menos um dos dois for true
+let podeAplicarAVagaOu= maiorDeIdade || possuiCarteiraDeTrabalho;
+console.log(podeAplicarAVagaOu)
+
+//Operador NOT (!) : inverte de ture para false e vice-versa
+let candidatoRecusado = !podeAplicarAVagaOu;
+
+console.log(candidatoRecusado)
