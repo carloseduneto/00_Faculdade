0afc4cfb30c12cc12dcff7d345653744fb0e80fa
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2022-10-18 00:16:07 -0300

    Update 05_fizzBuzz.js

--- 09 JavaScript Dev Aprender/05_fizzBuzz.js
+++ 09 JavaScript Dev Aprender/05_fizzBuzz.js
@@ -3,20 +3,25 @@
 //Divi´sivel por 3 e 5 => FizzBuzz
 //Não é divisível por 3 ou 5 => entrada
 //Não é um número => "Não é um número"
-const resultado = fizzBuzz (66);
+const resultado = fizzBuzz (15);
 console.log(resultado)
 
 function fizzBuzz (entrada){
-    if (entrada%3===0 && entrada%5===0){
-        console.log("FizzBuzz")
-    }else if(entrada%3===0){
-        console.log("Fizz")
+    if( typeof entrada !== 'number'){
+        // console.log("Não é um número!")
+        return "Não é um número";
+    }else if(entrada%3===0 && entrada%5===0){
+        // console.log("Fizz")
+        return "FizzBuzz";
     }else if(entrada%5===0){
-        console.log("Buzz")
-    }else if(entrada%3!=0 && entrada%5==0){
-        console.log(entrada)
+        // console.log("Buzz")
+        return "Buzz";
+    }else if(entrada%3===0){
+        // console.log("FizzBuzz")
+        return "Fizz"
     }else{
-        console.log("Não é um número!")
+        // console.log(entrada)
+        return entrada;
     }
     
 }
