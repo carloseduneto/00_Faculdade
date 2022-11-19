508fd6e2a535a800fd0ee5ae6ee0526cd140ece7
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2022-11-19 07:22:33 -0300

    Sistemas 2

--- 02 Estrutura de Dados/09_ReusoCodigos/Trabalho 1 ao 5/04_NumeroPorExtenso.py
+++ 02 Estrutura de Dados/09_ReusoCodigos/Trabalho 1 ao 5/04_NumeroPorExtenso.py
@@ -1,5 +1,9 @@
 n2=5
 backup=5
+unidades=["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"]
+dez=["dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
+dezenas=["zero","dez","vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
+centenas=["cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
 
 def menu():
     print(30 * '-')
@@ -36,6 +40,58 @@ def limpar():
     global n2, backup
     n2=backup
 
+def indexarDezenas(numero):
+    numero=numero/10
+    numero=int(numero)
+    return numero
+
+def indexarCentenas(numero):
+    numero=numero/100
+    numero=int(numero)
+    return numero    
+
+def valorCentenas(numero):
+    subtracao=numero-valorDezenas(numero)
+    subtracao=indexarCentenas(subtracao)
+    return subtracao
+
+
+def valorDezenas(numero):
+    subtracao=numero-restoDezenas(numero)
+    subtracao=indexarDezenas(subtracao)
+    return subtracao
+
+def restoDezenas(numero):
+    resto=numero%10
+    return resto
+
+def restoCentenas(numero):
+    resto=numero%100
+    return resto
+
+
+def retornarCentenas(numero):
+    numero=numero/100
+    numero=int(numero)
+    return numero
+
+def escreveDezenas(numero):
+    if numero>=10 and numero <= 19:
+        return dez[numero-10]
+        
+    elif numero>=20 and numero <=99:
+        if(numero%10==0):
+            numero=indexarDezenas(numero)
+            return dezenas[numero]
+        else:
+            algarismoDezenas=valorDezenas(numero)
+            return dezenas[algarismoDezenas]
+
+def escreveUnidades(numero):
+    algarismoUnidades=restoDezenas(numero)
+    return unidades[algarismoUnidades]
+
+
 
 # 
 # num_2 = int(input('Digite o segundo número: '))
@@ -48,7 +104,7 @@ def opcao():
         num_1 = int(input('Digite o primeiro número: '))
         menu()
         operacao = int(input('Escolha uma opção: '))
-        if operacao < 1 or operacao > 6:
+        if operacao < 1 or operacao > 7:
             print('Opção inválida!')
         elif operacao == 1:
             print(somar(num_1))
@@ -70,7 +126,67 @@ def opcao():
             print(limpar())
         
         elif operacao == 6:
-            numeroPorExtenso=int(input(""))    
+            escolha="S"
+            while escolha=="S":
+                numeroPorExtenso=int(input("Digite um valor:"))
+                # print(10**(len(str(numeroPorExtenso))))    
+                if numeroPorExtenso <= 999999999:
+                    if  numeroPorExtenso>=0 and numeroPorExtenso <=9:
+                        print(escreveUnidades(numeroPorExtenso))
+
+                    if numeroPorExtenso>=10 and numeroPorExtenso <=99:
+                        print(escreveDezenas(numeroPorExtenso))
+
+                    if numeroPorExtenso>=100 and numeroPorExtenso <=999:
+                        if (numeroPorExtenso==100):
+                            print("cem")
+                        elif(numeroPorExtenso%100==0):
+                            numero=indexarCentenas(numeroPorExtenso)
+                            numero=numero-1
+                            print(centenas[numero])
+                            
+                        else:
+                            if escreveDezenas(restoCentenas(numeroPorExtenso))==None:
+                                algarismoCentenas=valorCentenas(numeroPorExtenso)
+                                print(centenas[algarismoCentenas]," e ",escreveUnidades(restoDezenas(numeroPorExtenso)))
+                                
+                            elif restoDezenas(numeroPorExtenso)==0:
+                                algarismoCentenas=valorCentenas(numeroPorExtenso)
+                                print(centenas[algarismoCentenas], " e ", escreveDezenas(restoCentenas(numeroPorExtenso)))
+
+                            else:
+                                algarismoCentenas=valorCentenas(numeroPorExtenso)
+                                print(centenas[algarismoCentenas], " e ", escreveDezenas(restoCentenas(numeroPorExtenso)), " e ",escreveUnidades(restoDezenas(numeroPorExtenso)))
+
+
+                    if numeroPorExtenso>=1000 and numeroPorExtenso <=9999:
+                        pass
+
+                    if numeroPorExtenso>=10000 and numeroPorExtenso <= 99999:
+                        pass
+
+                    if numeroPorExtenso>=100000 and numeroPorExtenso <= 999999:
+                        pass
+
+                    if numeroPorExtenso>=1000000 and numeroPorExtenso<= 9999999:
+                        pass
+
+                    if numeroPorExtenso>=10000000 and numeroPorExtenso<= 99999999:
+                        pass
+
+                    if numeroPorExtenso>= 100000000 and numeroPorExtenso<=999999999:
+                        pass
+                            
+                            
+                    
+
+                    
+
+                
+                escolha=input("Deseja continuar a operação? <S> <N>").upper()
+
+                    
+
 
         elif operacao == 7:
             print('Finalizando o programa...')
@@ -79,3 +195,24 @@ def opcao():
 
             
 opcao()
+
+
+'''                    if numeroPorExtenso <=9:
+                        print(unidades[numeroPorExtenso])
+                    if numeroPorExtenso >=10 and numeroPorExtenso <=19:
+                        print(dez[numeroPorExtenso-10])
+                    outrasDezenas(numeroPorExtenso)
+                    cem(numeroPorExtenso)
+
+                    if numeroPorExtenso >100 and numeroPorExtenso<=999:
+                        if numeroPorExtenso%100==0:
+                            numero=retornarCentenas(numeroPorExtenso)
+                            print(centenas[numero])
+                        else:
+                            restoCentenas=numeroPorExtenso%100
+                            subtracaoCentenas=numeroPorExtenso-restoCentenas
+
+                            restoDezenas=subtracaoCentenas%10
+                            subtracaoDezenas=subtracaoCentenas-restoDezenas
+
+                            print()'''
